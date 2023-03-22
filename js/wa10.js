00442a4d0806295f7c24b9c43881408cbd837996
Evelyn Upchurch <evup8596@colorado.edu>
2023-03-22 17:32:38 -0600

    wa10 start and images

---
+++ js/wa10.js
@@ -0,0 +1,21 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+
+const images = ['Connor_and_I_Prom.jpg' , 'Gabe_Tyler_Gavin_Funny.jpg' , 'Brady_is_Funny.jpg' , 'Shay_and_I_Coffee.jpg' , 'Aidan_Gabe_Me_Skiing.jpg'];
+const alter = {}
+
+/* Declaring the alternative text for each image file */
+
+/* Looping through images */
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
