dc6d78b6eadb6ae5f98c40d5174b025976aafb09
Evelyn Upchurch <evup8596@colorado.edu>
2023-04-09 14:01:40 -0600

    wa11 finish stupid Anime api dont work

--- js/wa11.js
+++ js/wa11.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
         quote.textContent = data.content;
         cite.textContent = data.author;
     }else{
-        quote.textContent = "failed to fetch quote :(";
+        quote.textContent = "failed to fetch quote, this is quite sadge";
         console.log(data);
     }
 }
@@ -21,24 +21,28 @@ const twitterURL = "https://twitter.com/intent/tweet/";
 const linkTarget = '_top';
 const windowOptions = 'menubar=no,status=no,height=750,width=500';
 
-function extractQuote(){
-    return document.querySelector('#js-quote-text');
+function takeQuote(){
+    console.log(document.querySelector('#js-quote-text').textContent);
+    return document.querySelector('#js-quote-text').textContent;
+    
 }
 
-function extractAuthor(){
-    return document.querySelector('#js-quote-author');
+function citeAuthor(){
+    console.log(document.querySelector('#js-quote-author').textContent);
+    return document.querySelector('#js-quote-author').textContent;
 }
 
-function openTwitterWindow(text, link) {
-    const twitterQuery = `text=${text}&url=${link}`;
-    return window.open(`${twitterUrl}?${twitterQuery}&`, linkTarget, windowOptions);
+function openTwitterWindow() {
+    const quote = extractQuote();
+    const author = extractAuthor();
+    const twitterQuery = `text=${encodeURIComponent(`"${quote}" - ${author}`)}&url=`;
+    return window.open(`${twitterURL}?${twitterQuery}&`, linkTarget, windowOptions);
 }
 
 function shareQuote() {
-    const quote = extractQuote()
-    const author = extractAuthor();
     const twitterButton = document.querySelector('#js-tweet-button');
-    twitterButton.addEventListener('click', () => openTwitterWindow(quote, author))
+    twitterButton.addEventListener('click', () => openTwitterWindow());
 }
 
-shareQuote()
+getQuote();
+shareQuote();
