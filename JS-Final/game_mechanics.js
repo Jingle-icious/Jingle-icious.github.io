c30074855ddeb3ec0aac126150580179559e2450
Evelyn Upchurch <evup8596@colorado.edu>
2023-05-02 18:01:59 -0600

    JS final finished i think

--- JS-Final/game_mechanics.js
+++ JS-Final/game_mechanics.js
@@ -76,7 +76,7 @@ function drawBall() {
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
-  ctx.closePath(); // ball is drawn
+  ctx.closePath(); 
 }
 
 function drawPaddle() {
